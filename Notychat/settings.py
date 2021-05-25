046eba83a407099edd48ba5a87a26a1f0ad9a5a1
mohitCodepy <mohit1511199@gmail.com>
2021-05-26 02:13:26 +0530

    added chat logic

--- Notychat/settings.py
+++ Notychat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "Your Secret Key Here"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
