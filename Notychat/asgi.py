aaa99d3de228c366d6116399cf2bc3bb6eb36b85
mohitCodepy <mohit1511199@gmail.com>
2021-05-26 02:30:32 +0530

    added chat logic

--- Notychat/asgi.py
+++ Notychat/asgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
-import os
+# import os
 
-from django.core.asgi import get_asgi_application
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Notychat.settings')
+
+# application = get_asgi_application()
+
+import os
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Notychat.settings')
 
-application = get_asgi_application()
+django.setup()
+
+application = get_default_application()
