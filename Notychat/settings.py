a1fc10e7e947b03167c732038577122be2600ce0
mohitCodepy <mohit1511199@gmail.com>
2021-06-08 19:27:21 +0530

    serving media files

--- Notychat/settings.py
+++ Notychat/settings.py
@@ -126,7 +126,7 @@ TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
-USE_L10N = False
+USE_L10N = True
 
 USE_TZ = True
 
--- Notychat/urls.py
+++ Notychat/urls.py
@@ -19,10 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls.conf import re_path
+from django.views.static import serve
 urlpatterns = [
     path('mohitwhatsadmin100/', admin.site.urls),
     path('',include('core.urls')),
     path('auth/',include('users.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
