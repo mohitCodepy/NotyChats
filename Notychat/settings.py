f884586d84069e3516a54ee1983a0635cf07b833
mohitCodepy <61349019+mohitCodepy@users.noreply.github.com>
2021-06-09 16:59:51 +0530

    settings updated

--- Notychat/settings.py
+++ Notychat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "Your Secret Key Here"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
