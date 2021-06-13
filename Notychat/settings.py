6f126b9d7eef806813025559d8f7df6f8eee6a43
mohitCodepy <mohit1511199@gmail.com>
2021-06-13 20:06:23 +0530

    new changes

--- Notychat/settings.py
+++ Notychat/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'users.apps.UsersConfig',
-    'authentication.apps.AuthenticationConfig'
+
 ]
 
 MIDDLEWARE = [
